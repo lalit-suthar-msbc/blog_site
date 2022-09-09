1989dcc3abaca3b22645d9822e0dd1bdd5335bab
lsuthar123 <Lalit.Suthar@MSBC-MAINFRAME.LCL>
2022-09-09 18:34:52 +0100

    files added

--- Blog/form.py
+++ Blog/form.py
@@ -1,15 +1,16 @@
 from .models import Account
 from django.forms import ModelForm
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import authenticate
 from django.forms import forms
+
+
 class UserModel(ModelForm):
     class Meta:
-        model=Account
-        fields=["first_name","last_name","email","username","password"]
+        model = Account
+        fields = ["first_name", "last_name", "email", "username", "password"]
 
-    def clean(self):
-        if self.is_valid():
-            username = self.cleaned_data['username']
-            password = self.cleaned_data['password']
-            if not authenticate(email=username, password=password):
-                raise forms.ValidationError("Invalid Login")
+# class UserModel(UserCreationForm):
+#     class Meta:
+#         model=Account
+#         fields=["first_name","last_name","email","username","password"]
--- Blog/views.py
+++ Blog/views.py
@@ -4,28 +4,52 @@ from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login
 from .form import UserModel
 from django.contrib.auth import logout
-
+from django.contrib.auth import get_user_model
+from .models import Account
 
 # Create your views here.
 def home(request):
+
     if request.method == "POST":
 
         fr = UserModel(request.POST)
+        print(fr)
         if fr.is_valid():
-            fr.save()
+            email=fr.cleaned_data.get("email")
+            first_name=fr.cleaned_data.get("first_name")
+            last_name=fr.cleaned_data.get("last_name")
+            username = fr.cleaned_data.get('username')
+            password = fr.cleaned_data.get('password')
+            password1 = request.POST.get('password1')
+            print("user is created successfully")
+            user=Account.objects.create_user(email=email,username=username,password=password)
+            user.save()
 
-            print("user is saved successfully")
+
+            # user = authenticate(username=username, password=password)
+
+            # if request.user.is_authenticated:
+            #     print(f"{request.user.username} is logged in ")
+            #     print("user is logged in successfully")
     return render(request, "demo.html")
 
 
 def login_user(request):
+    # User_ = get_user_model()
+    # data=User_.objects.all()
+    # for i in data:
+    #     u = User.objects.get(username=i)
+    #     u.delete()
+
     if request.method == "POST":
-        name = request.POST["username"]
-        ps = request.POST["password"]
-        usr = authenticate(username=name, password=ps)
+        email = request.POST.get("email")
+        ps = request.POST.get("password")
+        usr = authenticate(email=email, password=ps)
         login(request, usr)
         name = request.user.username
-        return render(request, "user_is_valid.html", {"name": name})
+        firstname=request.user.first_name
+        print(firstname)
+        return render(request, "user_is_valid.html", {"name": name,"firstname":firstname})
         # return HttpResponse(f"{request.user.username} is logged in")
 
     return render(request, "login.html")
